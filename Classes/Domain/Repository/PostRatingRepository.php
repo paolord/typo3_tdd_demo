1c331b1c709ccaa105c057b63369dcac227b1158
Angelo <angelo@deskma.com>
2018-12-07 18:20:12 +0800

    criteria3 done

--- Classes/Domain/Repository/PostRatingRepository.php
+++ Classes/Domain/Repository/PostRatingRepository.php
@@ -10,5 +10,45 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
  * @author Angelo Obispo <angelo at deskma.com>
  */
 class PostRatingRepository extends Repository {
-    //put your code here
+    
+    /**
+     * inserts a like record in postrating table
+     * 
+     * @param int $postId
+     * @param int $userId
+     */
+    public function like ($postId, $userId) {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_tdddemo_domain_model_postrating');
+               
+        $queryBuilder
+            ->insert('tx_tdddemo_domain_model_postrating')
+            ->values([
+                'post_id' => $postId,
+                'user_id' => $userId,
+                'like' => 1,
+                'dislike' => 0
+            ])
+            ->execute();
+    }
+    
+        
+    /**
+     * inserts a dislike record in postrating table
+     * 
+     * @param int $postId
+     * @param int $userId
+     */
+    public function dislike ($postId, $userId) {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_tdddemo_domain_model_postrating');
+               
+        $queryBuilder
+            ->insert('tx_tdddemo_domain_model_postrating')
+            ->values([
+                'post_id' => $postId,
+                'user_id' => $userId,
+                'like' => 0,
+                'dislike' => 1
+            ])
+            ->execute();
+    }
 }
--- Tests/Unit/Domain/Repository/PostRatingRepositoryTest.php
+++ Tests/Unit/Domain/Repository/PostRatingRepositoryTest.php
@@ -2,6 +2,8 @@
 namespace Angelo\TddDemo\Tests\Unit\Domain\Repository;
 
 use Nimut\TestingFramework\TestCase\UnitTestCase;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 
 /**
  * Description of PostRatingRepositoryTest
@@ -14,10 +16,129 @@ class PostRatingRepositoryTest extends UnitTestCase {
     protected $objectManager;
     
     /**
+     *
+     * @var \Angelo\TddDemo\Domain\Repository\PostRatingRepository
+     * @inject
+     */
+    protected $prr;
+            
+    public function setUp()
+    {
+        $this->objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
+        $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = [
+           'charset' => 'utf8',
+           'dbname' => 'test',
+           'driver' => 'mysqli',
+           'host' => 'localhost',
+           'password' => '',
+           'unix_socket' => '',
+           'user' => 'deskma'
+        ];
+        $connectionPool = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class);
+        $postConnection =  $connectionPool->getConnectionForTable('tx_tdddemo_domain_model_post');
+        $postConnection->bulkInsert(
+            'tx_tdddemo_domain_model_post',
+            [
+               ['content' => 'Test Post 1'],
+               ['content' => 'Test Post 2']
+            ],
+            [
+               'content'
+            ]
+         );
+        
+        $userConnection =  $connectionPool->getConnectionForTable('tx_tdddemo_domain_model_users');
+        $userConnection->bulkInsert(
+            'tx_tdddemo_domain_model_users',
+            [
+               ['username' => 'user1']
+            ],
+            [
+               'username'
+            ]
+         );
+        
+    }
+    
+    public function tearDown()
+    {
+        $connection = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Database\ConnectionPool::class);
+        $connection->getConnectionForTable('tx_tdddemo_domain_model_postrating')->truncate('tx_tdddemo_domain_model_postrating');
+        $connection->getConnectionForTable('tx_tdddemo_domain_model_post')->truncate('tx_tdddemo_domain_model_post');
+        $connection->getConnectionForTable('tx_tdddemo_domain_model_users')->truncate('tx_tdddemo_domain_model_users');
+    }
+
+    /**
      * @test
      */
     public function postRatingRepositoryClassExists() {
         $this->assertTrue(class_exists(\Angelo\TddDemo\Domain\Repository\PostRatingRepository::class));
     }
+    
+    /**
+     * @test
+     */
+    public function likeMethod() {
+        $objectManager = $this->getMockBuilder('TYPO3\\CMS\\Extbase\\Object\\ObjectManagerInterface')->getMock();
+        $postRatingRepository = $this->getAccessibleMock(\Angelo\TddDemo\Domain\Repository\PostRatingRepository::class, ['dummy'], [$objectManager]);;
+        
+        $postRatingRepository->like(1 , 1);
+        
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_tdddemo_domain_model_postrating');
+        $like = $queryBuilder
+            ->select('tx_tdddemo_domain_model_postrating.like')
+            ->from('tx_tdddemo_domain_model_postrating')
+            ->where(
+                $queryBuilder->expr()->like(
+                    'tx_tdddemo_domain_model_postrating.post_id',
+                    1
+                )
+            )
+            ->andWhere(
+                $queryBuilder->expr()->like(
+                    'tx_tdddemo_domain_model_postrating.user_id',
+                    1
+                )
+            )
+            ->orderBy($be_user_table.'.uid', 'DESC')
+            ->execute()
+            ->fetchColumn(0);
+        
+        $this->assertEquals(1 , $like);
+    }
+    
+    
+    /**
+     * @test
+     */
+    public function dislikeMethod() {
+        $objectManager = $this->getMockBuilder('TYPO3\\CMS\\Extbase\\Object\\ObjectManagerInterface')->getMock();
+        $postRatingRepository = $this->getAccessibleMock(\Angelo\TddDemo\Domain\Repository\PostRatingRepository::class, ['dummy'], [$objectManager]);;
+        
+        $postRatingRepository->dislike(2 , 1);
+        
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_tdddemo_domain_model_postrating');
+        $dislike = $queryBuilder
+            ->select('tx_tdddemo_domain_model_postrating.dislike')
+            ->from('tx_tdddemo_domain_model_postrating')
+            ->where(
+                $queryBuilder->expr()->like(
+                    'tx_tdddemo_domain_model_postrating.post_id',
+                    2
+                )
+            )
+            ->andWhere(
+                $queryBuilder->expr()->like(
+                    'tx_tdddemo_domain_model_postrating.user_id',
+                    1
+                )
+            )
+            ->orderBy($be_user_table.'.uid', 'DESC')
+            ->execute()
+            ->fetchColumn(0);
+        
+        $this->assertEquals(1 , $dislike);
+    }
+    
 
 }
